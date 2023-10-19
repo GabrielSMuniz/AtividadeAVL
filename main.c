e4be7d1478ef19b0758684c0433b4f3e9c6609d4
Gabriel de Souza Muniz <65411267+GabrielSMuniz@users.noreply.github.com>
2023-10-19 18:13:13 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "bst.h"
+
+int main()
+{
+  Tree t = createTree();
+
+  AVLinsert(t, 15);
+  AVLinsert(t, 5);
+  AVLinsert(t, 7);
+  AVLinsert(t, 32);
+  AVLinsert(t, 45);
+
+  imprime(t,"teste");
+
+  return 0;
+}
